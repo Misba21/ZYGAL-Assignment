0a1b68d738edebc5aa72ef7b46f9d0b9c46efed7
Misba21 <118809277+Misba21@users.noreply.github.com>
2022-11-22 15:21:18 +0530

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,22 @@
+﻿const btn1 = document.getElementById('btn1');
+const btn2 = document.getElementById('btn2');
+
+const block1 = document.getElementById('box1');
+const block2 = document.getElementById('box2');
+const block3 = document.getElementById('box3');
+const block4 = document.getElementById('box4');
+
+btn1.addEventListener('click', function onClick(){
+block1.style.backgroundColor='orange';
+block1.style.backgroundColor='red';
+block1.style.backgroundColor='yellow';
+block1.style.backgroundColor='green';
+});
+
+btn2.addEventListener('click', function onClick(){
+    block1.style.backgroundColor='red';
+    block1.style.backgroundColor='yellow';
+    block1.style.backgroundColor='green';
+    block1.style.backgroundColor = 'orange';
+    
+});
